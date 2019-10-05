1f882b55df9c2c005b4eea3d4ad2c36fb439eddd
Eduardo Ugolini <edu.ugolini2@gmail.com>
2019-10-04 23:42:30 -0300

    Adicionado suporte a seta para voltar a tela

--- src/containers/RegisterHeader.vue
+++ src/containers/RegisterHeader.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="header-box">
-        <h1>Cadastro</h1>
-        <h2>{{ subtitle }}</h2>
+        <div class="back-button" v-if="backButton">
+            <img src="@/assets/up-arrow.svg">
+        </div>
+        <div>
+            <h1>Cadastro</h1>
+            <h2>{{ subtitle }}</h2>
+        </div>
     </div>
 </template>
 
@@ -12,6 +17,11 @@ export default {
         subtitle: {
             type: String,
             required: true
+        },
+        backButton: {
+            type: String,
+            required: false,
+            default: () => ''
         }
     }
 }
@@ -20,5 +30,35 @@ export default {
 <style scoped>
     .header-box {
         margin: 20px 0 20px 0;
+        display: flex;
+        position: relative;
+        text-align: center;
+        justify-content: center;
+    }
+
+    .back-button {
+        width: 25px;
+        transform: rotate(270deg);
+        position: absolute;
+        left: 15px;
+        color: #C2D6D7;
+    }
+
+    h1 {
+        font-family: Source Sans Pro;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 24px;
+        margin: unset;
+        color: #222222;
+    }
+
+    h2 {
+        font-family: Source Sans Pro;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        color: #C2D6D7;
+        margin: unset;
     }
 </style>
