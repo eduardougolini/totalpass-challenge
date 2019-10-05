18ec25aa07d787bbfd8f32f147534868674a9714
Eduardo Ugolini <edu.ugolini2@gmail.com>
2019-10-04 23:58:59 -0300

    Adicionado componente do rodapé

---
+++ src/components/FooterSteps.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="footer">
+        <div 
+            class="dot" 
+            v-for="step in totalSteps"
+            :class="{ active: step === actualStep }"
+        ></div>
+    </div>
+</template>
+    
+<script>
+export default {
+    name: 'FooterSteps',
+    props: {
+        totalSteps: {
+            type: Number,
+            required: false,
+            default: () => 3
+        },
+        actualStep: {
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .footer {
+        display: flex;
+        justify-content: center;
+    }
+
+    .dot {
+        height: 5px;
+        width: 5px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+    }
+
+    .dot.active {
+        background-color: #00BA9F;
+    }
+</style>
--- src/pages/BasicDataRegister.vue
+++ src/pages/BasicDataRegister.vue
@@ -2,18 +2,21 @@
     <div>
         <RegisterHeader :subtitle="'Dados Pessoais'" />
         <RegisterDataForm />
+        <FooterSteps :actualStep="1" />
     </div>
 </template>
 
 <script>
     import RegisterHeader from '@/containers/RegisterHeader';
     import RegisterDataForm from '@/containers/RegisterDataForm';
+    import FooterSteps from '@/components/FooterSteps';
 
     export default {
         name: 'BasicDataRegister',
         components: {
             RegisterHeader,
-            RegisterDataForm
+            RegisterDataForm,
+            FooterSteps
         }
     }
 </script>
