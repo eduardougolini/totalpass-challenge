49c6fd549b20f9cacffba461f9f8fee638ada59a
Eduardo Ugolini <edu.ugolini2@gmail.com>
2019-10-08 11:00:12 -0300

    Corrigido bug no cadastro de endereço

--- src/containers/RegisterAddressForm.vue
+++ src/containers/RegisterAddressForm.vue
@@ -2,54 +2,54 @@
   <div class="container">
     <form class="address-form">
       <CustomInput
-        v-model="cep"
+        v-model="$v.cep.$model"
         :name="'CEP'"
         :customClasses="{
-          'error': this.$v['addressModel'].cep.$dirty
-            && this.$v['addressModel'].cep.$invalid
+          'error': $v.cep.$dirty
+            && $v.cep.$invalid
         }"
         :format="'#####-###'"
       />
       <CustomInput
-        v-model="addressModel.address"
+        v-model="$v.address.$model"
         :name="'Endereço'"
         :customClasses="{
-          'error': this.$v['addressModel'].address.$dirty
-            && this.$v['addressModel'].address.$invalid
+          'error': $v.address.$dirty
+            && $v.address.$invalid
         }"
       />
       <CustomInput
-        v-model="addressModel.number"
+        v-model="$v.number.$model"
         :name="'Número'"
         :customClasses="{
-          'error': this.$v['addressModel'].number.$dirty
-            && this.$v['addressModel'].number.$invalid
+          'error': $v.number.$dirty
+            && $v.number.$invalid
         }"
       />
       <CustomInput
-        v-model="addressModel.complement"
+        v-model="complement"
         :name="'Complemento'"
         :isRequired="false"
       />
       <CustomInput
-        v-model="addressModel.district"
+        v-model="district"
         :name="'Bairro'"
         :isRequired="false"
       />
       <CustomInput
-        v-model="addressModel.city"
+        v-model="$v.city.$model"
         :name="'Cidade'"
         :customClasses="{
-          'error': this.$v['addressModel'].city.$dirty
-            && this.$v['addressModel'].city.$invalid
+          'error': $v.city.$dirty
+            && $v.city.$invalid
         }"
       />
       <CustomInput
-        v-model="addressModel.state"
+        v-model="$v.state.model"
         :name="'Estado'"
         :customClasses="{
-          'error': this.$v['addressModel'].state.$dirty
-            && this.$v['addressModel'].state.$invalid
+          'error': $v.state.$dirty
+            && $v.state.$invalid
         }"
         :inputType="'select'"
         :selectOptions="brazilianStates"
@@ -72,7 +72,13 @@ export default {
   name: 'RegisterAddressForm',
   data() {
     return {
-      addressInfo: {},
+      cep: '',
+      address: '',
+      number: '',
+      complement: '',
+      district: '',
+      city: '',
+      state: '',
     };
   },
   components: {
@@ -80,51 +86,47 @@ export default {
     CustomInput,
   },
   computed: {
-    ...mapState(TOTAL_PASS, [
-      'addressData',
-      'brazilianStates',
-    ]),
-    addressModel: {
-      get() {
-        return {
-          ...this.addressData,
-          ...this.addressInfo,
-        };
-      },
-      set(val) {
-        this.addressInfo = val;
-      },
-    },
-    cep: {
-      get() {
-        return this.addressModel.cep;
-      },
-      async set(val) {
-        const formatedCep = this.getFormattedCep(val);
-
-        if (formatedCep.length !== 8) {
-          return;
-        }
-
-        const cepInfo = await this.fetchCepData(formatedCep);
-
-        if (cepInfo) {
-          this.addressModel = {
-            cep: formatedCep,
-            address: cepInfo.logradouro || '',
-            number: '',
-            complement: '',
-            district: cepInfo.bairro || '',
-            city: cepInfo.localidade || '',
-            state: cepInfo.uf || '',
-          };
-        }
-      },
-    },
+    ...mapState(TOTAL_PASS, {
+      cepState: state => state.addressData.cep,
+      addressState: state => state.addressData.address,
+      numberState: state => state.addressData.number,
+      complementState: state => state.addressData.complement,
+      districtState: state => state.addressData.district,
+      cityState: state => state.addressData.city,
+      stateState: state => state.addressData.state,
+      brazilianStates: state => state.brazilianStates,
+    }),
+    
   },
   async created() {
+    this.cep = this.cepState;
+    this.address = this.addressState;
+    this.number = this.numberState;
+    this.complement = this.complementState;
+    this.district = this.districtState;
+    this.city = this.cityState;
+    this.state = this.stateState;
+
     if (this.brazilianStates.length < 2) {
       await this.loadBrazilianStates();
+    }
+  },
+  watch: {
+    async cep(val) {
+      const formatedCep = this.getFormattedCep(val);
+
+      if (formatedCep.length !== 8) {
+        return;
+      }
+
+      const cepInfo = await this.fetchCepData(formatedCep);
+
+      if (cepInfo) {
+        this.address = cepInfo.logradouro || '';
+        this.district = cepInfo.bairro || '';
+        this.city = cepInfo.localidade || '';
+        this.state = cepInfo.uf || '';
+      };
     }
   },
   methods: {
@@ -143,28 +145,34 @@ export default {
         return;
       }
 
-      this.setAddressData(this.addressModel);
+      this.setAddressData({
+        cep: this.cep,
+        address: this.address,
+        number: this.number,
+        complement: this.complement,
+        district: this.district,
+        city: this.city,
+        state: this.state,
+      });
 
       this.$router.push({ name: 'PlansList' });
     },
   },
   validations: {
-    addressModel: {
-      cep: {
-        required,
-      },
-      address: {
-        required,
-      },
-      number: {
-        required,
-      },
-      city: {
-        required,
-      },
-      state: {
-        required,
-      },
+    cep: {
+      required,
+    },
+    address: {
+      required,
+    },
+    number: {
+      required,
+    },
+    city: {
+      required,
+    },
+    state: {
+      required,
     },
   },
 };
